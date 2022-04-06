68b0303c56405d539a613f4c69aff9de35663785
William-Rome4 <william_franz2002@hotmail.com>
2022-04-06 15:18:47 -0300

    Bucket-S3

---
+++ bucketS3.auto.tfvars
@@ -0,0 +1,2 @@
+region = "us-east-2"
+s3_bucket_name = "s3-william"
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  type = string
+  default = "us-east-2"
+}
+
+variable "s3_bucket_name" {
+  type = string
+}
