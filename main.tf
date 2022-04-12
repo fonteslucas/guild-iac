6478fe91f00bdf312f160443cd026aa92616c2d0
William Franz Piossek - 125111345448 <125111345448@ulife.com.br>
2022-04-12 20:57:09 -0300

    Desafios-13/04

--- main.tf
+++ main.tf
@@ -7,4 +7,21 @@ resource "random_string" "random" {
 
 resource "aws_s3_bucket" "bucketS3" {
   bucket = "${var.s3_bucket_name}-${random_string.random.result}"
+
+  tags = var.s3_bucket_tags
+}
+
+resource "aws_security_group" "securityGroup" {
+  name = var.sg_name
+}
+
+resource "aws_security_group_rule" "sgRule" {
+  security_group_id = aws_security_group.securityGroup.id
+  type = "ingress"
+  from_port = 8080
+  to_port = 8090
+  protocol = "tcp"
+  description = "Service Ports"
+  cidr_blocks = var.sg_rule_cidr
+  self = true
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,28 @@
 variable "region" {
+  description = "AWS Region where resources will be deployed"
   type = string
   default = "us-east-2"
 }
 
 variable "s3_bucket_name" {
+  description = "Name of the S3 bucket"
   type = string
+}
+
+variable "s3_bucket_tags" {
+  description = "Tags applied to the S3 bucket"
+  type = map(object({
+    Name = string
+    Owner = string
+  }))
+}
+
+variable "sg_name" {
+  description = "Security Group Name"
+  type = string
+}
+
+variable "sg_rule_cidr" {
+  description = "SG Rules CIDR Blocks"
+  type = list(string)
 }
