3ad26d2284a767879f3d7e7a20bbe1c7d592be96
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2022-04-17 02:28:50 -0300

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -7,16 +7,3 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
-
-# Time, os dois desafios pro fds~quarta
-
-
-
-# Desafio 1 - Usando Variavel Map
-# No mesmo recurso do S3, adicionar tags utilizando variáveis do tipo map.
-
-# Name = value
-# Owner = value
-# Desafio 2 - Usando Variavel List
-# Criar SG e um SG rule. Na SG rule personalizar os cidr_blocks 
-#usando uma variavel do tipo lista.
