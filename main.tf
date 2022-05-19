db83ffb4988d2fe70ea273265d8f3d095c9c84bf
Dallison Lima <dallison.lima@brlink.com.br>
2022-05-19 15:53:47 -0300

    fmt

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ module "vpc" {
 
   source = "terraform-aws-modules/vpc/aws"
 
-  name                 = var.name-vpc
-  cidr                 = var.cidr-vpc
-  azs                  = data.aws_availability_zones.azs.names
-  private_subnets      = slice(cidrsubnets(var.cidr-vpc, 4, 4, 4), 0, 3)
-  public_subnets       = slice(cidrsubnets(var.cidr-vpc, 4, 4, 4, 4, 4, 4), 3, 6)
+  name            = var.name-vpc
+  cidr            = var.cidr-vpc
+  azs             = data.aws_availability_zones.azs.names
+  private_subnets = slice(cidrsubnets(var.cidr-vpc, 4, 4, 4), 0, 3)
+  public_subnets  = slice(cidrsubnets(var.cidr-vpc, 4, 4, 4, 4, 4, 4), 3, 6)
 
 
   tags = {
